ad89687172cf557224a34bbee963304dfdcf9e7a
joshhawks1823 <jheat23@gmail.com>
2020-02-22 04:05:31 -0500

    heroku push

--- src/helpers/enviroment.js
+++ src/helpers/enviroment.js
@@ -7,7 +7,7 @@ switch (window.location.hostname) {
     APIURL = "http://localhost:4000";
     break;
   // this is the deployed react application
-  case "https://kdp-inventory-server.herokuapp.com/":
+  case "kdp-inventory-server.herokuapp.com":
     // this is the full url of your deployed API
     APIURL = "https://kdp-inventory-server.herokuapp.com/";
 }
--- src/home/Navbar.js
+++ src/home/Navbar.js
@@ -16,31 +16,6 @@ import {
 
 import Signup from "../components/Signup";
 
-// const Sitebar = props => {
-//   const [isOpen, setIsOpen] = useState(false);
-//   const toggle = () => {
-//     let newIsOpen = !isOpen;
-//     setIsOpen(newIsOpen);
-//   };
-//   return (
-//     <Navbar color="faded" light expand="md">
-//       <Container className="openContainer">
-//         <NavbarBrand href="/">
-//           <h3 className="alignHeader">KDP Inventory</h3>
-//         </NavbarBrand>
-//         <NavbarToggler onClick={toggle} />
-//         <Collapse isOpen={isOpen} navbar>
-//           <Nav className="ml-auto" navbar>
-//             <NavItem>
-//               {/* <Button onClick={props.clickLogout}>Logout</Button> */}
-//             </NavItem>
-//           </Nav>
-//         </Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// };
-
 const Sitebar = props => {
   const [isOpen, setIsOpen] = useState(false);
 
