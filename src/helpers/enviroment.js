fb7b2377be31bee12c043d1c367fcab38352ef2c
joshhawks1823 <jheat23@gmail.com>
2020-02-22 05:20:43 -0500

    heroku push

--- src/helpers/enviroment.js
+++ src/helpers/enviroment.js
@@ -4,12 +4,12 @@ switch (window.location.hostname) {
   // this is the local host name of your react app
   case "localhost" || "127.0.0.1":
     // this is the local host name of your API
-    APIURL = "http://localhost:4000";
+    APIURL = "https://localhost:4000";
     break;
   // this is the deployed react application
   case "kdp-inventory-server.herokuapp.com":
     // this is the full url of your deployed API
-    APIURL = "http://kdp-inventory-server.herokuapp.com/";
+    APIURL = "https://kdp-inventory-server.herokuapp.com/";
 }
 
 export default APIURL;
