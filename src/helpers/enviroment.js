507c73eb92a52ea042682b429f5beecddf75cb81
joshhawks1823 <jheat23@gmail.com>
2020-02-22 03:40:36 -0500

    new commit

--- src/helpers/enviroment.js
+++ src/helpers/enviroment.js
@@ -7,9 +7,9 @@ switch (window.location.hostname) {
     APIURL = "http://localhost:4000";
     break;
   // this is the deployed react application
-  case "https://kdp-inventory-server.herokuapp.com":
+  case "https://kdp-inventory-app.herokuapp.com/":
     // this is the full url of your deployed API
-    APIURL = " https://kdp-inventory-server.herokuapp.com/";
+    APIURL = "https://kdp-inventory-server.herokuapp.com/";
 }
 
 export default APIURL;
