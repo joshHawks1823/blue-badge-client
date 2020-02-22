f61e9396f34be4331483f2bbd7f0d7918de2a14f
joshhawks1823 <jheat23@gmail.com>
2020-02-22 05:16:42 -0500

    heroku push

--- src/helpers/enviroment.js
+++ src/helpers/enviroment.js
@@ -9,7 +9,7 @@ switch (window.location.hostname) {
   // this is the deployed react application
   case "kdp-inventory-server.herokuapp.com":
     // this is the full url of your deployed API
-    APIURL = "https://kdp-inventory-server.herokuapp.com/";
+    APIURL = "http://kdp-inventory-server.herokuapp.com/";
 }
 
 export default APIURL;
